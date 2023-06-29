759010072b260728d9489790d460fc5d4e884531
John Lynch <jjlynch314@gmail.com>
2023-06-28 21:03:00 -0400

    update

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: 'https://api.yelp.com/v3/businesses',
   headers: {
     Authorization:
-      'Bearer eiZFcJrVd4M67caIt9faFuw-Ioitr3RjkPyMSiFGOejbSc4ZVqiQhEXJM24Mw4_nPwA3w7oKpeAc94fSOS6HUR3HJxe2Iqi-IeuPlUtPerZgNFs8W18PN1uYc2OCZHYx'
+      'Bearer $$##'
   }
 });
